7f68e99795ff173f9d75123f8d47940ca5fbf79d
Zhu <fjejunior@gmail.com>
2019-01-26 03:09:19 -0200

    Creating design for home page

--- home.php
+++ home.php
@@ -8,11 +8,40 @@
 <html>
     <head>
         <title>Welcome</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/home.css">
     </head>
-    <body></body>
+    <body>
+        <div class="container"> 
+            <div class="container-sup">
+                <label>HappyDays App</label>
+                <hr>
+                <p>The HappyDays App is a web app for you to save and read the daily happiness memories.</p>
+            </div>
+            <div class="container-sub">
+                <div class="funcs">
+                    <a href="New Memories">
+                        <i class="far fa-sticky-note"></i>
+                        <label>New Memories</label>
+                    </a>
+                </div>
+                <div class="funcs">
+                    <a href="New Memories">
+                        <i class="far fa-sticky-note"></i>
+                        <label>Sorted Memories</label>
+                    </a>
+                </div>
+                <div class="funcs">
+                    <a href="New Memories">
+                        <i class="far fa-sticky-note"></i>
+                        <label>View Memories</label>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
 
 <?php
